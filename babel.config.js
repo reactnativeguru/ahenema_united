378c40c4b5259aa97f98016e8eb7f79793b3e58c
Talha javed <39768820+Talha-Jawed@users.noreply.github.com>
2023-02-24 21:50:29 +0500

    packages added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
 };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
+    },
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+    ],
+  };
+};
