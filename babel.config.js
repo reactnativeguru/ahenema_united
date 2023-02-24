0d2ba852999bf97de1827d09f93ce9d3f6756e70
Talha javed <39768820+Talha-Jawed@users.noreply.github.com>
2023-02-25 00:05:20 +0500

    build issue fixed

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    env: {
-      production: {
-        plugins: ['transform-remove-console'],
-      },
-    },
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
-    ],
-  };
+  plugins: ['react-native-reanimated/plugin'],
 };
